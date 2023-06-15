92c78e5c36657883bc21b6fd7e528ac86067c238
Usenmfon <usenmfonuko@gmail.com>
2023-06-15 09:20:12 +0100

    fix: made config module global

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
+import { ConfigModule } from '@nestjs/config';
 
-const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/todo-app'
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/todo-app';
 @Module({
   imports: [
-    MongooseModule.forRoot(MONGO_URL, {
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URL, {
       connectionFactory: (connection: Connection) => {
         return connection;
       },
